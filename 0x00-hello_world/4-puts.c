2efbe5d56bb79ebacc2dfa828f1e812841b09541
manar mamdouh <manarmamdouh122@gmail.com>
2022-09-09 09:34:26 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,22 @@
-#!/bin/bash
+#!/Bin/Bash
 #include <stdio.h>
 
 
 
+/**
 
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+
+ * Return: 0 if exited properly, non-zero otherwise
+
+ */
 
 int main(void)
 
 {
-    
-    puts("\"Programming is like building a multilingual puzzle");
-    
-    return (0);
-    
-}
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+} 
