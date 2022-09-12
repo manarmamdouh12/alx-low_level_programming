bd7d85e0b13bfc69e38e91ca25e8d0a5d01009ca
manar mamdouh <110994393+manarmamdouh12@users.noreply.github.com>
2022-09-12 15:24:46 +0200

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,45 +1,19 @@
 #include <stdio.h>
-
-
-
 /**
-
- * main - print the latterss of the alphabet
-
- *
-
- * Description: print the letters of the alphabet in reverse
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-
-
+* main - print the latterss of the alphabet
+*
+* Description: print the letters of the alphabet in reverse
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-  
 {
-  
-  int i = 122;
-  
-
-  
-  while (i > 96)
-    
-    {
-      
-      putchar(i);
-      
-      i--;
-      
-    }
-  
-  putchar(10);
-  
-
-  
-  return (0);
-  
+int i = 122;
+while (i > 96)
+{
+putchar(i);
+i--;
+}
+putchar(10);
+return (0);
 }
