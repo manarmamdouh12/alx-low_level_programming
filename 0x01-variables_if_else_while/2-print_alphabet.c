80e34f486fc8449e1a0100de878283902549a1f1
manar mamdouh <manarmamdouh122@gmail.com>
2022-09-12 02:49:03 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,49 @@
+able File  23 lines (19 sloc)  264 Bytes
+  
+
+  
+#include <stdio.h>
+  
+
+  
+/**
+
+ * main - print the letters of the alphabet
+
+ *
+
+ * Description: print the letters of the alphabet
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+  
+
+  
+  int main(void)
+  
+{
+  
+  int i = 97;
+  
+
+  
+  while (i < 123)
+    
+    {
+      
+      putchar(i);
+      
+      i++;
+      
+    }
+  
+  putchar(10);
+  
+
+  
+  return (0);
+  
+}
